c3cc031ac8c13a7d881af550ca4c761a7bb4171b
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-20 20:10:14 +0900

    AppNavi仮実装 #1

---
+++ src/components/AppNavi.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export interface MenuItem {
+  type: "heading" | "menu";
+  title: string;
+  icon?: string;
+  url?: string;
+}
+</script>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+defineProps<{ title: string; menuItems: MenuItem[] }>();
+
+const router = useRouter();
+
+const isToggle = ref(false);
+
+const gotoUrl = (url?: string) => {
+  if (url != undefined) {
+    router.push(url);
+  }
+};
+</script>
+<template>
+  <router-view></router-view>
+</template>
