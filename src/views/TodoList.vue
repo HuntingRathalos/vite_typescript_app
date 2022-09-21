ac60fe57c7ffc574caa466f4ed1625c9d4efff47
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-21 16:56:42 +0900

    TodoList仮実装 #1

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,3 +1,19 @@
-<template>
-  <h1>TodoList</h1>
-</template>
+<script setup lang="ts">
+import { reactive } from "vue";
+import { Task } from "../models/Task";
+import TaskAdd from "../components/TaskAdd.vue";
+import TaskList from "../components/TaskList.vue";
+
+const tasks: Task[] = reactive([
+  {
+    id: 1,
+    title: "起きる",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "きがえる",
+    done: false,
+  },
+]);
+</script>
