76a08671f1605e4174a2a103b5c1f8c82fdfc90d
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-23 10:29:38 +0900

    piniaインストール #3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import "./style.css";
 import "./index.css";
-import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "./router";
+import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(createPinia).use(router).mount("#app");
