c84618170015d21c5744445eec32c273f441bd90
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-25 11:30:10 +0900

    pinia-plugin-persistインストール #3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
-import "./style.css";
 import "./index.css";
 import { createPinia } from "pinia";
 import router from "./router";
+import piniaPersist from "pinia-plugin-persist";
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+const pinia = createPinia();
+pinia.use(piniaPersist);
+
+createApp(App).use(pinia).use(router).mount("#app");
