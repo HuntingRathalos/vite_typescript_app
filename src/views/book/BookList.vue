82450f94867a95123a5f88a73ef9846c08551063
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-26 10:20:37 +0900

    検索フォームのplaceholder変更 #3

--- src/views/book/BookList.vue
+++ src/views/book/BookList.vue
@@ -22,7 +22,7 @@ const search = (e: Event) => {
         type="text"
         id="keyword"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="ONEPIECE"
+        placeholder="検索キーワードを入力してね"
         required
       />
       <button
