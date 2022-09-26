df674f32d85d5942000d9fc256f7433a95a8f07d
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-26 10:18:52 +0900

    DetailItemのスタイル調整 #3

--- src/components/book/DetailItem.vue
+++ src/components/book/DetailItem.vue
@@ -14,30 +14,28 @@ const sendBookPage = () => {
 <template>
   <section class="text-gray-600 body-font">
     <div
-      class="container mx-auto flex px-5 py-24 items-center justify-center flex-col"
+      class="container mx-auto flex px-5 py-16 items-center justify-center flex-col"
     >
-      <a @click="sendBookPage">
-        <img
-          class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-          alt=""
-          :src="book.image"
-        />
-        <div class="text-center lg:w-2/3 w-full">
-          <h1
-            class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
+      <img
+        class="lg:w-2/6 md:w-3/6 mb-10 object-cover object-center rounded text-center"
+        :src="book.image"
+      />
+      <div class="text-center w-full">
+        <h2
+          class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
+        >
+          {{ book.title }}
+        </h2>
+        <p class="mb-8 leading-relaxed">{{ book.description }}</p>
+        <div class="flex justify-center">
+          <button
+            @click="sendBookPage"
+            class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
-            {{ book.title }}
-          </h1>
-          <p class="mb-8 leading-relaxed">{{ book.description }}</p>
-          <div class="flex justify-center">
-            <button
-              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            >
-              Button
-            </button>
-          </div>
+            詳細を見に行く
+          </button>
         </div>
-      </a>
+      </div>
     </div>
   </section>
 </template>
